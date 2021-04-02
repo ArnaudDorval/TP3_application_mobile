83703c912a12e1a53133cb87d4691d8d1a9d7052
Arnaud Dorval <arnaud.dorval-leblanc.1@ulaval.ca>
2021-04-02 13:05:02 -0400

    offerOutput et activity description

---
+++ TP3/app/src/main/java/ca/ulaval/ima/tp3/model/OfferOutput.kt
@@ -0,0 +1,18 @@
+package ca.ulaval.ima.tp3.model
+
+import com.google.gson.annotations.SerializedName
+
+data class OfferOutput(
+    @SerializedName("id") val id:Int,
+    @SerializedName("model")  val model: Model,
+    @SerializedName("image")  val image:String,
+    @SerializedName("description")  val description:String,
+    @SerializedName("seller")  val seller:Account,
+    @SerializedName("year")  val year:Int,
+    @SerializedName("from_owner")  val from_owner:Boolean,
+    @SerializedName("kilometers")  val kilometers:Int,
+    @SerializedName("transmission")  val transmission:String,
+    @SerializedName("price")  val price:Int,
+    @SerializedName("created")  val created:String,
+    )
+
